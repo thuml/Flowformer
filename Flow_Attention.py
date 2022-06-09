cde2da0887585fcdbfc23073f1f36f33a80422df
wuhaixu2016 <wuhaix2016@163.com>
2022-06-09 21:07:48 +0800

    init code

---
+++ Flow_Attention.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+
+
+## Core code for Flow-Attention, Please refer to each folder for corresponding experiments
+
+class Flow_Attention(nn.Module):
+    # flow attention in normal version
+    def __init__(self, d_input, d_model, d_output, n_heads, drop_out=0.05, eps=1e-6):
+        super(Flow_Attention, self).__init__()
+        self.n_heads = n_heads
+        self.query_projection = nn.Linear(d_input, d_model)
+        self.key_projection = nn.Linear(d_input, d_model)
+        self.value_projection = nn.Linear(d_input, d_model)
+        self.out_projection = nn.Linear(d_model, d_output)
+        self.dropout = nn.Dropout(drop_out)
+        self.eps = eps
+
+    def kernel_method(self, x):
+        return torch.sigmoid(x)
+
+    def dot_product(self, q, k, v):
+        kv = torch.einsum("nhld,nhlm->nhdm", k, v)
+        qkv = torch.einsum("nhld,nhdm->nhlm", q, kv)
+        return qkv
+
+    def forward(self, queries, keys, values):
+        ## input: B (L or S) D; output: B L D
+        ## Note: queries, keys, values are not projected yet
+        ## 1. Linear projection
+        B, L, _ = queries.shape
+        _, S, _ = keys.shape
+        queries = self.query_projection(queries).view(B, L, self.n_heads, -1)
+        keys = self.key_projection(keys).view(B, S, self.n_heads, -1)
+        values = self.value_projection(values).view(B, S, self.n_heads, -1)
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        # 2. Non-negative projection
+        queries = self.kernel_method(queries)
+        keys = self.kernel_method(keys)
+        ## 3. Flow-Attention
+        # (1) Calculate incoming and outgoing flow
+        sink_incoming = 1.0 / (torch.einsum("nhld,nhd->nhl", queries + self.eps, keys.sum(dim=2) + self.eps))
+        source_outgoing = 1.0 / (torch.einsum("nhld,nhd->nhl", keys + self.eps, queries.sum(dim=2) + self.eps))
+        # (2) conservation refine for source and sink
+        conserved_sink = torch.einsum("nhld,nhd->nhl", queries + self.eps,
+                                      (keys * source_outgoing[:, :, :, None]).sum(dim=2) + self.eps)
+        conserved_source = torch.einsum("nhld,nhd->nhl", keys + self.eps,
+                                        (queries * sink_incoming[:, :, :, None]).sum(dim=2) + self.eps)
+        conserved_source = torch.clamp(conserved_source, min=-10.0, max=10.0)  # for stability
+        # (3) Competition & Allocation
+        sink_allocation = torch.sigmoid(conserved_sink * (float(queries.shape[2]) / float(keys.shape[2])))
+        source_competition = torch.softmax(conserved_source, dim=-1) * float(keys.shape[2])
+        # (4) dot product
+        x = (self.dot_product(queries * sink_incoming[:, :, :, None],  # for value normalization
+                              keys,
+                              values * source_competition[:, :, :, None])  # competition
+             * sink_allocation[:, :, :, None]).transpose(1, 2)  # allocation
+        ## (5) Final projection
+        x = x.reshape(B, L, -1)
+        x = self.out_projection(x)
+        x = self.dropout(x)
+        return x
+
+
+class Flow_Attention_Causal(nn.Module):
+    # flow attention in causal version
+    def __init__(self, d_input, d_model, d_output, n_heads, drop_out=0.05, eps=1e-6):
+        super(Flow_Attention_Causal, self).__init__()
+        self.n_heads = n_heads
+        self.query_projection = nn.Linear(d_input, d_model)
+        self.key_projection = nn.Linear(d_input, d_model)
+        self.value_projection = nn.Linear(d_input, d_model)
+        self.out_projection = nn.Linear(d_model, d_output)
+        self.dropout = nn.Dropout(drop_out)
+        self.eps = eps
+
+    def kernel_method(self, x):
+        return torch.sigmoid(x)
+
+    def causal_dot_product(self, q, k, v):
+        kv = torch.einsum("nhld,nhlm->nhldm", k, v)
+        kv = torch.cumsum(kv, dim=2)
+        qkv = torch.einsum("nhld,nhldm->nhlm", q, kv)
+        return qkv
+
+    def forward(self, queries, keys, values):
+        ## input: B (L or S) D; output: B L D
+        ## Note: queries, keys, values are not projected yet
+        ## 1. Linear projection
+        B, L, _ = queries.shape
+        _, S, _ = keys.shape
+        queries = self.query_projection(queries).view(B, L, self.n_heads, -1)
+        keys = self.key_projection(keys).view(B, S, self.n_heads, -1)
+        values = self.value_projection(values).view(B, S, self.n_heads, -1)
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        # 2. Non-negative projection
+        queries = self.kernel_method(queries)
+        keys = self.kernel_method(keys)
+        ## 3. Causal Flow-Attention
+        # (1) Calculate incoming and outgoing flow
+        sink_incoming = 1.0 / (torch.einsum("nhld,nhld->nhl", queries + self.eps, keys.cumsum(dim=2) + self.eps))
+        source_outgoing = 1.0 / (torch.einsum("nhld,nhld->nhl", keys + self.eps, queries.cumsum(dim=2) + self.eps))
+        # approximate normal conservation col and row by multiplying corresponding element number
+        normal = (((torch.arange(queries.shape[2])).float() + 1.0)).to(queries.device)[None, None, :]
+        sink_incoming = sink_incoming * normal
+        source_outgoing = source_outgoing * normal
+        # (2) conservation refine for source and sink
+        conserved_sink = torch.einsum("nhld,nhld->nhl", queries + self.eps,
+                                      (keys * source_outgoing[:, :, :, None]).cumsum(dim=2) + self.eps) / normal
+        conserved_source = torch.einsum("nhld,nhld->nhl", keys + self.eps,
+                                        (queries * sink_incoming[:, :, :, None]).cumsum(
+                                            dim=2) + self.eps) / normal
+        conserved_source = torch.clamp(conserved_source, min=-10.0, max=10.0)  # for stability
+        # (3) Competition & Allocation
+        sink_allocation = torch.sigmoid(conserved_sink)
+        conserved_source = torch.exp(conserved_source)
+        source_competition = (conserved_source / conserved_source.cumsum(dim=-1)) * normal
+        # (4) Causal dot product
+        x = (self.causal_dot_product(queries * sink_incoming[:, :, :, None],  # for value normalization
+                                     keys,
+                                     values * source_competition[:, :, :, None])  # competition
+             * sink_allocation[:, :, :, None]).transpose(1, 2)  # allocation
+        ## (5) Final projection
+        x = x.reshape(B, L, -1)
+        x = self.out_projection(x)
+        x = self.dropout(x)
+        return x
+
+
+if __name__ == '__main__':
+    # just for simple test
+    attn_normal = Flow_Attention(10, 16, 16, 4)
+    attn_causal = Flow_Attention_Causal(10, 16, 16, 4)
+    x = torch.rand([1, 100, 10])
+    x_attn_normal = attn_normal(x, x, x)
+    x_attn_causal = attn_causal(x, x, x)
+    assert x_attn_normal.shape == (1, 100, 16)
+    assert x_attn_causal.shape == (1, 100, 16)
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Flowformer
-About About Code release for "Flowformer: Linearizing Transformers with Conservation Flows" (ICML 2022), https://arxiv.org/pdf/2202.06258.pdf
+# Flowformer (ICML 2022)
+Flowformer: Linearizing Transformers with Conservation Flows
+
+Transformers have achieved impressive success in various areas. However, the attention mechanism has a quadratic complexity, significantly impeding Transformers from dealing with numerous tokens and scaling up to bigger models. In pursuing the **linear complexity** and **task-universal** foundation model, we propose Flowformer [[paper]](https://arxiv.org/pdf/2202.06258.pdf) with the following merits:
+
+- **Linear complexity** w.r.t sequence length, can hand extermely long sequence (4k+ tokens)
+- **Without specific indcitve bias**, purely derived from the flow network theory
+- **Task-universal**, show strong performance in **Long sequence, Vision, NLP, Time series, RL**
+
+## Flow-Attention Design
+
+We cast the attention mechanism into flow network, where the information flow is aggregated from the sources (values) to the sinks (results) through the learned flow capacities (attentions).
+
+By conducting the conservation in both source and sink ascpects, we can bring competition into Flow-Attention design to avoid trivial attention in the spirit that "fixed resource will cause competition''.
+
+<p align="center">
+<img src=".\pic\Flow-Attention.png" height = "250" alt="" align=center />
+<br><br>
+<b>Figure 1.</b> Flow-Attention with Competition and Allocation mechanisms.
+</p>
+
+## Get Started
+
+1. Please refer to different folders for detailed experiment instructions.
+
+   Note: We have suffered a lot in configuring environments for different tasks. If you also have problems in solving the environment, feel free to contact us.
+
+2. Progress of benchmarks (All the code will be released before 2022.07.30)
+
+- [x] Core code: see `Flow_Attention.py`
+- [ ] Long Sequence Modeling in LRA
+- [ ] Vision Recognization in ImageNet-1K
+- [ ] Language Modeling in WikiText-103
+- [ ] Time series classification  in UEA
+- [ ] Reinforcement Leanring in D4RL
+
+## Main Results
+
+See the [[paper]](https://arxiv.org/pdf/2202.06258.pdf) for detailed results, including nearly 20 comparing baselines. 
+
+| Task                                | Metric                                               | Flowformer       | Performer    | Reformer     | Vanilla<br>Transformer |
+| ----------------------------------- | ---------------------------------------------------- | ---------------- | ------------ | ------------ | ---------------------- |
+| Long Sequence in LRA                | Avg Acc (%) $\uparrow$                               | **56.48**        | 51.41        | 50.67        | OOM                    |
+| Vision Recognization in ImageNet-1K | Top-1 Acc (%) $\uparrow$                             | **80.4**         | 78.1         | 79.6         | 78.7                   |
+| Language Modeling in WikiText-103   | Perplexity $\downarrow$                              | **30.8**         | 37.5         | 33.6         | 33.0                   |
+| Time series classification in UEA   | Avg Acc (%)                                          | **73.0**         | 71.5         | 71.9         | 71.9                   |
+| Offline RL in D4RL                  | Avg Reward $\uparrow$ <br>Avg deviation $\downarrow$ | 71.1$\pm$**2.1** | 63.8$\pm$7.6 | 58.3$\pm$9.5 | **72.2**$\pm$2.6       |
+
+Vanilla Transformer means Decision Transorfomer in RL.
+
+## Attention Visualziation
+
+<p align="center">
+<img src=".\pic\Attention-visualization.png" height = "300" alt="" align=center />
+<br><br>
+<b>Figure 2.</b> Attention visualization. Flow-Attention can capture the essential parts successively.
+</p>
+
+## Citation
+
+If you find this repo useful, please cite our paper. 
+
+```
+@inproceedings{wu2022flowformer,
+  title={Flowformer: Linearizing Transformers with Conservation Flows},
+  author={Haixu Wu and Jialong Wu and Jiehui Xu and Jianmin Wang and Mingsheng Long},
+  booktitle={International Conference on Machine Learning},
+  year={2022}
+}
+```
+
+## Contact
+
+If you have any questions or want to use the code, please contact whx20@mails.tsinghua.edu.cn.
